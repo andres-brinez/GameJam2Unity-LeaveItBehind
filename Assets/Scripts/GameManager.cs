15ac1d9bfbec892a1e07782b36fff463761f3a2d
Andres Briñez <brinezlopez08@gmail.com>
2025-03-12 14:00:51 -0500

    Updated Level 1 scene with minimap, safe zone, and GameManager integration.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,7 +23,6 @@ public class GameManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(this.gameObject);
             Debug.Log("Game manager se intancio");
         }
         else
